9d3044221f48654fe4689ffd699ce7c7e7ed6435
Mark Nelson <mnelson@redhat.com>
2016-08-18 13:05:00 -0500

    make append take const parameter.
    Signed-off-by: Mark Nelson <mnelson@redhat.com>

--- src/include/buffer.h
+++ src/include/buffer.h
@@ -434,7 +434,7 @@ namespace buffer CEPH_BUFFER_API {
     public:
       unsafe_appender(bufferlist *l, size_t s) : appender(l, s) {}
       unsafe_appender(appender *parent) : appender(parent) {}
-      void append(char *p, size_t l) {
+      void append(const char *p, size_t l) {
 	memcpy(pos, p, l);
 	pos += l;
       }
@@ -448,7 +448,7 @@ namespace buffer CEPH_BUFFER_API {
     class safe_appender : public appender {
     public:
       safe_appender(bufferlist *l, size_t s) : appender(l, s) {}
-      void append(char *p, size_t l) {
+      void append(const char *p, size_t l) {
 	if (pos + l > end) {
 	  flush();
 	  prepare_buffer(l);
