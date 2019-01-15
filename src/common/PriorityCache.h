yehudasa/ceph-pr-archive

common/PriorityCache: Implement a Cache Manager
Signed-off-by: Mark Nelson <mnelson@redhat.com>

src/common/PriorityCache.h
@@ -17,9 +17,27 @@
 
 #include <stdint.h>
 #include <string>
+#include <memory>
 #include <vector>
+#include <unordered_map>
+#include "common/perf_counters.h"
+#include "include/assert.h"
 
 namespace PriorityCache {
+  // Reserve 16384 slots for PriorityCache perf counters
+  const int PERF_COUNTER_LOWER_BOUND = 1073741824;
+  const int PERF_COUNTER_MAX_BOUND = 1073758208;
+
+  enum MallocStats {
+    M_FIRST = PERF_COUNTER_LOWER_BOUND,
+    M_TARGET_BYTES,
+    M_MAPPED_BYTES,
+    M_UNMAPPED_BYTES,
+    M_HEAP_BYTES,
+    M_CACHE_BYTES,
+    M_LAST,
+  };
+
   enum Priority {
     PRI0,
     PRI1,
@@ -34,6 +52,12 @@ namespace PriorityCache {
     PRI10,
     PRI11,
     LAST = PRI11,
+  };
+
+  enum Extra {
+    E_RESERVED = Priority::LAST+1,
+    E_COMMITTED,
+    E_LAST = E_COMMITTED,
   };
 
   int64_t get_chunk(uint64_t usage, uint64_t total_bytes);
@@ -88,7 +112,42 @@ namespace PriorityCache {
 
     // Get intervals
     virtual uint64_t get_intervals(PriorityCache::Priority pri) const = 0;
-;
+  };
+
+  struct Manager {
+    CephContext* cct = nullptr;
+    PerfCounters* logger;
+    std::unordered_map<std::string, PerfCounters*> loggers;
+    std::unordered_map<std::string, std::vector<int>> indexes;
+    std::unordered_map<std::string, std::shared_ptr<PriCache>> caches;
+
+    // Start perf counter slots after the malloc stats.
+    int cur_index = MallocStats::M_LAST;
+
+    uint64_t min_mem = 0;
+    uint64_t max_mem = 0;
+    uint64_t target_mem = 0;
+    uint64_t tuned_mem = 0;
+
+    Manager(CephContext *c, uint64_t min, uint64_t max, uint64_t target);
+    ~Manager();
+    void set_min_memory(uint64_t min) {
+      min_mem = min;
+    }
+    void set_max_memory(uint64_t max) {
+      max_mem = max;
+    }
+    void set_target_memory(uint64_t target) {
+      target_mem = target;
+    }
+    uint64_t get_tuned_mem() const {
+      return tuned_mem;
+    }
+    void insert(std::string name, std::shared_ptr<PriCache> c);
+    void erase(std::string name);
+    void tune_memory();
+    void balance();
+    void balance_priority(int64_t *mem_avail, Priority pri);
   };
 }
 

markhpc/ceph

src/common/PriorityCache.h
@@ -17,9 +17,27 @@
 
 #include <stdint.h>
 #include <string>
+#include <memory>
 #include <vector>
+#include <unordered_map>
+#include "common/perf_counters.h"
+#include "include/ceph_assert.h"
 
 namespace PriorityCache {
+  // Reserve 16384 slots for PriorityCache perf counters
+  const int PERF_COUNTER_LOWER_BOUND = 1073741824;
+  const int PERF_COUNTER_MAX_BOUND = 1073758208;
+
+  enum MallocStats {
+    M_FIRST = PERF_COUNTER_LOWER_BOUND,
+    M_TARGET_BYTES,
+    M_MAPPED_BYTES,
+    M_UNMAPPED_BYTES,
+    M_HEAP_BYTES,
+    M_CACHE_BYTES,
+    M_LAST,
+  };
+
   enum Priority {
     PRI0,
     PRI1,
@@ -34,6 +52,12 @@ namespace PriorityCache {
     PRI10,
     PRI11,
     LAST = PRI11,
+  };
+
+  enum Extra {
+    E_RESERVED = Priority::LAST+1,
+    E_COMMITTED,
+    E_LAST = E_COMMITTED,
   };
 
   int64_t get_chunk(uint64_t usage, uint64_t total_bytes);
@@ -88,7 +112,42 @@ namespace PriorityCache {
 
     // Get intervals
     virtual uint64_t get_intervals(PriorityCache::Priority pri) const = 0;
-;
+  };
+
+  struct Manager {
+    CephContext* cct = nullptr;
+    PerfCounters* logger;
+    std::unordered_map<std::string, PerfCounters*> loggers;
+    std::unordered_map<std::string, std::vector<int>> indexes;
+    std::unordered_map<std::string, std::shared_ptr<PriCache>> caches;
+
+    // Start perf counter slots after the malloc stats.
+    int cur_index = MallocStats::M_LAST;
+
+    uint64_t min_mem = 0;
+    uint64_t max_mem = 0;
+    uint64_t target_mem = 0;
+    uint64_t tuned_mem = 0;
+
+    Manager(CephContext *c, uint64_t min, uint64_t max, uint64_t target);
+    ~Manager();
+    void set_min_memory(uint64_t min) {
+      min_mem = min;
+    }
+    void set_max_memory(uint64_t max) {
+      max_mem = max;
+    }
+    void set_target_memory(uint64_t target) {
+      target_mem = target;
+    }
+    uint64_t get_tuned_mem() const {
+      return tuned_mem;
+    }
+    void insert(std::string name, std::shared_ptr<PriCache> c);
+    void erase(std::string name);
+    void tune_memory();
+    void balance();
+    void balance_priority(int64_t *mem_avail, Priority pri);
   };
 }
 