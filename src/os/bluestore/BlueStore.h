967b39dbe00f101e784a6ff92c4d263278acb9e0
Gabriel BenHanokh <benhanokh@gmail.com>
2022-03-10 17:40:31 +0200

    os/bluestore: Disable NCB functionality on rotational drives NCB code needs to recover allocation map after an OSD crash. The recovery process on rotational drives is about 20x slower than SSD making this solution unacceptable for that environment 
    Signed-off-by: Gabriel Benhanokh <gbenhano@redhat.com>
    (cherry picked from commit 5fd09658edbf636dd462facfa9878656f641e7de)

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -2791,6 +2791,7 @@ public:
 
   bool is_rotational() override;
   bool is_journal_rotational() override;
+  bool is_db_rotational() ;
 
   std::string get_default_device_class() override {
     std::string device_class;
